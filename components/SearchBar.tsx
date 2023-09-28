8f02a4bbe71f795347d5a3fbe5d659911d87c5a4
Hostnomics <hostnomics@gmail.com>
2023-09-27 23:56:43 -0400

    Updated SearchBar.tsx with search inputs

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,13 +6,38 @@
 import {SearchManufacturer}  from "./";
 import { useState } from "react";
 
+import Image from 'next/image';
+
+//Add SearchButton "component" here since we'll only be using it in SearchBar.tsx (3:33:55)
+//Can't use {} because 'SearchButton' cannot be used as a JSX component
+    // const SearchButton = ({ otherClasses }:  otherClasses: string }) => {
+    //   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+
+    //   </button>
+    // }
+
+// Use () for immediate return arrow function instead:
+    const SearchButton = ({ otherClasses }:  {otherClasses: string }) => (
+      <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+        <Image 
+          src="/magnifying-glass.svg"
+          width={40}
+          height={40}
+          alt="magnifying glass"
+          className="object-contain"
+        />
+      </button>
+    )
+
 
 
 const SearchBar = () => {
-
     const [manufacturer, setManufacturer] = useState('');
+    const [model, setModel] = useState('');
+
 
     const handleSearch = () => {
+        //update cars we are showing by updating the fetch call to our API (3:33:30): https://youtu.be/A6g8xc0MoiY?si=dI6QPADENQymPNJX&t=12810
 
     }
 
@@ -24,7 +49,34 @@ const SearchBar = () => {
                 manufacturer={manufacturer}
                 setManufacturer={setManufacturer}
             />
+
+            <SearchButton otherClasses="sm:hidden" />
         </div>
+
+
+{/* Below search button, create another div for the second input  */}
+        <div className="searchbar__item">
+            <Image 
+              src="/model-icon.png"
+              width={25}
+              height={25}
+              className="absolute w-[20px] h-[20px] ml-4"
+              alt="car model"
+            />
+
+            <input 
+              type="text"
+              name="model"
+              value={model}
+              onChange={(e) => setModel(e.target.value)}
+              placeholder="Tiguan"
+              className="searchbar__input"
+            />
+
+            <SearchButton otherClasses="sm:hidden"/>
+        </div>
+            <SearchButton otherClasses="max-sm:hidden"/>
+
     </form>
   )
 }
